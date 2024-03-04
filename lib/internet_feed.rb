b251a4aedcd7d265a771132f20ba7d4e1da06be3
Shane Becker <veganstraightedge@gmail.com>
2024-03-03 21:43:12 -0800

    Wrap up administrivia tests

--- lib/internet_feed.rb
+++ lib/internet_feed.rb
@@ -2,5 +2,4 @@ require_relative 'internet_feed/version'
 
 module InternetFeed
   class Error < StandardError; end
-  # Your code goes here...
 end
--- spec/internet_feed_spec.rb
+++ spec/internet_feed_spec.rb
@@ -1,5 +1,11 @@
 RSpec.describe InternetFeed do
-  it 'has a version number' do
-    expect(InternetFeed::VERSION).not_to be_nil
+  describe 'Administrivia' do
+    it 'has a version number' do
+      expect(InternetFeed::VERSION).not_to be_nil
+    end
+
+    it 'has the correct class name' do
+      expect(described_class.name).to eq 'InternetFeed'
+    end
   end
 end
