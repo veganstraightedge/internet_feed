74eabeb94f457dd8af0d636948677dc33223fe10
Shane Becker <veganstraightedge@gmail.com>
2024-03-03 20:59:26 -0800

    Disable Style/FrozenStringLiteralComment

--- spec/internet_feed_spec.rb
+++ spec/internet_feed_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 RSpec.describe InternetFeed do
   it 'has a version number' do
     expect(InternetFeed::VERSION).not_to be_nil
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'internet_feed'
 
 RSpec.configure do |config|
